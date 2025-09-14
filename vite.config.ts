f787beb4741b3e7ee807905d629469c0a81d4329
gift.mkwara <gift.mkwara@phocassoftware.com>
2025-09-15 11:20:56 +1200

    fixed the base url in vite config, resolving it from the package json name

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
+import { name } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: `/${name}/`,
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
