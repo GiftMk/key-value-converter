6881e10e58cb82916b171755894c7c4bd9263c74
JeremyBertenshaw <jeremy.bertenshaw@phocassoftware.com>
2025-09-18 10:43:02 +1200

    Update header title to 'Awesome Key-Value Converter!'

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
 		<header className='flex w-full items-center justify-center gap-2'>
-			<h1 className='font-excalifont text-xl'>Key-Value Converter</h1>
+			<h1 className='font-excalifont text-xl'>Awesome Key-Value Converter!</h1>
 		</header>
 	);
 };
